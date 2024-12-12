4fbb909005c5b0821ab44108ccabd38a5828eed3
willydlw <willydlw@comcast.net>
2024-12-12 13:53:10 -0700

    part 1 not working. Keeping more information than needed to help with debugging

--- 2024/day04/day04.cpp
+++ 2024/day04/day04.cpp
@@ -10,6 +10,26 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <stack>
+
+const std::string TARGET_WORD{"XMAS"};
+
+
+struct Location{
+    int row;
+    int col;
+    Location(): row(0), col(0) {}
+    Location(int r, int c): row(r), col(c) {}
+};
+
+struct Word{
+    std::string letters;                // store target letters as they are found
+    Location gridLocations[4];          // store locations of target letters found
+    Location currentLocation;           // search neighbors of this location to find
+                                        // next letter
+    int numFound;                       // number of target letters found
+    char nextLetter;                    // next letter to find in word search
+};
 
 
 void populate_puzzle(std::string fileName, std::vector<std::vector<char>>& puzzle)
@@ -30,27 +50,114 @@ void populate_puzzle(std::string fileName, std::vector<std::vector<char>>& puzzl
     }
 }
 
-
-void remove_letters(std::vector<std::vector<char>>&puzzle)
+void add_border_to_puzzle(std::vector<std::vector<char>>& puzzle)
 {
-    for(size_t r = 0; r < puzzle.size(); r++)
-    {
-        for(size_t c = 0; c < puzzle[r].size(); c++){
-            switch(puzzle[r][c]){
-                case 'X':
-                break;
-                case 'M':
-                break;
-                case 'A':
-                break;
-                case 'S':
-                break;
-                default:
-                    std::cerr << "Unexpected puzzle letter: " << puzzle[r][c] 
-                        << ", at row: " << r << ", col: " << c << "\n";
+    int numRows = puzzle.size() + 2;
+    int numCols = puzzle[0].size() + 2;
+    std::vector<std::vector<char>> puzzCopy(numRows, std::vector<char>(numCols, '.'));
+    for(int i = 1; i < numRows-1; i++){
+        for(int j = 1; j < numCols-1; j++){
+            puzzCopy[i][j] = puzzle[i-1][j-1];
+        }
+    }
+
+    puzzle = puzzCopy;
+
+}
+
+
+std::vector<Location> checkNeighbors(const std::vector<std::vector<char>>& puzzle,
+        const Word* searchWord)
+{
+    std::vector<Location> locations;
+    int r = searchWord->currentLocation.row;
+    int c = searchWord->currentLocation.col;
+
+    for(int i = -1; i <= 1; i++){
+        for(int j = -1; j <= 1; j++){
+            if(puzzle[r+i][c+i] == searchWord->nextLetter){
+                locations.push_back((Location){r+i,c+i});
             }
         }
     }
+
+    return locations;
+}
+
+void find_words(std::vector<std::vector<char>>& puzzle, 
+                Word* startWord, std::vector<Word>& wordsFound)
+{
+    std::stack<Word> searchStack;
+    searchStack.push(*startWord);
+
+    while(!searchStack.empty()){
+
+        Word searchWord = searchStack.top();
+        searchStack.pop();
+
+        std::vector<Location> neighbors = checkNeighbors(puzzle, &searchWord);
+
+        for(auto n : neighbors){
+            Word temp = searchWord;
+
+            // add the letter found to the word
+            temp.letters.append(1, temp.nextLetter);
+
+            // add location of letter found
+            temp.gridLocations[temp.numFound] = n;
+
+            temp.currentLocation = n;
+
+            temp.numFound += 1;
+
+            if(temp.numFound < (int)TARGET_WORD.length()){
+                temp.nextLetter = TARGET_WORD[temp.numFound];
+                searchStack.push(temp);
+            }
+            else {
+                // found target word
+                wordsFound.push_back(temp);
+            }
+        }
+    }
+}
+
+
+/* Constraints:
+        puzzle contains at least one row
+        all rows have same number of columns
+        all letters are upper case
+*/
+
+
+int part01(std::vector<std::vector<char>>& puzzle)
+{
+    int wordCount = 0;
+    int numRows = puzzle.size();
+    int numCols = puzzle[0].size();
+    std::vector<Word> words;
+
+    for(int r = 0; r < numRows; r++){
+        for(int c = 0; c < numCols; c++){
+            if(puzzle[r][c] == 'X'){
+                Word word;
+                word.letters = "X";
+                word.currentLocation = (Location){r,c};
+                word.gridLocations[0] = (Location){r,c};
+                word.numFound = 1;
+                word.nextLetter = 'M';
+                
+                std::vector<Word> wordsFound;
+                find_words(puzzle, &word, wordsFound);
+                wordCount += wordsFound.size();
+
+                std::cout << "Words found for X at r: " << r << ", c: "
+                    << c << ", count: " << wordsFound.size() << "\n";
+            }
+        }
+    }
+
+    return wordCount;
 }
 
 
@@ -74,9 +181,19 @@ int main(int argc, char* argv[])
 
     std::vector<std::vector<char>> puzzle;
     populate_puzzle(fileName, puzzle);
+    
 
     std::cout << "\nPuzzle Input\n";
     print_puzzle(puzzle);
+
+    std::cout << "\nPuzzle with Border\n";
+    add_border_to_puzzle(puzzle);
+    print_puzzle(puzzle);
+
+
+    int xmasCount1 = part01(puzzle);
+
+    std::cout << "part 1: " << xmasCount1 << "\n";
 
     return 0;
 }
