cfb305a3b97323f6226dc6618517c975301debb4
willydlw <willydlw@comcast.net>
2024-12-14 10:21:09 -0700

    finally took the time to write out the matching pattern. Included it in comments so I will understand when I revisit code another day

--- 2024/day04/day04.cpp
+++ 2024/day04/day04.cpp
@@ -1,8 +1,7 @@
 /*  Part 1
         example.txt 18
-        input.txt
+        input.txt   2483
 
-    
 */
 
 
@@ -59,6 +58,85 @@ bool inbounds(int r, int c, int numRows, int numCols){
         puzzle contains at least one row
         all rows have same number of columns
         all letters are upper case
+
+
+        Word to left
+        ============
+        word index  3       2       1       0
+                    S       A       M       X
+        grid index  r,c-3   r,c-2,  r, c-1  r,c     dr = 0      dc = -1
+
+        Word to right
+        ============+
+        word index  0       1       2       3
+                    X       M       A       S
+        grid index  r,c     r,c+1,  r, c+2  r,c+3   dr = 0      dc = -1
+
+
+        Word above
+        ============
+                                Grid    Word
+                                Index   Index       dr = -1     dc = 0
+                            S   r-3,c   3           r + dr * i  c + dc * i
+                            A   r-2,c   2           
+                            M   r-1,c   1
+                            X   r-0,c   0
+        
+        Word below
+        ============
+                                Grid    Word
+                                Index   Index       dr = 1      dc = 0
+                            X   r+0,c   0           r + dr * i  c + dc * i
+                            M   r+1,c   1
+                            A   r+2,c   2
+                            S   r+3,c   3
+
+
+        Word diagonal up right
+        =======================
+
+                            Grid    Word
+                            Index   Index       dr = -1     dc = 1
+                    S       r-3,c+3   3
+                A           r-2,c+2   2
+            M               r-1,c+1   1
+        X                   r  ,c     0
+
+
+        Word diagonal up left
+        =======================
+
+                            Grid    Word
+                            Index   Index     dr = -1         dc = -1
+        S                   r-3,c-3   3
+            A               r-2,c-2   2
+                M           r-1,c-1   1
+                    X       r  ,c     0
+
+
+        Word diagonal down left
+        =======================
+
+                            Grid    Word
+                            Index   Index     dr = 1           dc = -1   
+                    X       r  ,c     0       r + dr * i       c + dc * i
+                M           r+1,c-1   1
+            A               r+2,c-2   2
+        S                   r+3,c-3   3
+
+        Word diagonal down right
+        =======================
+
+                            Grid    Word
+                            Index   Index       dr = 1          dc = 1
+        X                   r  ,c     0         r + dr * i      c + dc * i
+            M               r+1,c+1   1         r + dr * i      c + dc * i 
+                A           r+2,c+2   2         r + dr * i      c + dc * i
+                    S       r+3,c+3   3         r + dr * i      c + dc * i
+
+                            
+        Check all grid positions for character 'X'
+        When 'X' is found, must check all neighbors
 */
 int part01(std::vector<std::vector<char>>& puzzle)
 {
@@ -70,21 +148,22 @@ int part01(std::vector<std::vector<char>>& puzzle)
     for(int r = 0; r < numRows; r++){
         for(int c = 0; c < numCols; c++){
             if(puzzle[r][c] == 'X'){
-                // neighbor rows are r-1, r, r+1
+                // neighboring rows change by -1, 0, +1
                 for(int dr = -1; dr <= 1; dr++){
                     // neighbor cols are c-1, c, c+1
                     for(int dc = -1; dc <= 1; dc++){
+                        if(dr == 0 && dc == 0){
+                            continue;
+                        }
                         bool match = true;
-                        for(int i = 0; i < numChars && match; i++){
-                            if(inbounds(r+dr*i, c+dc*i, numRows, numCols)){
-                                if(puzzle[r+dr*i][c+dc*i] != XMAS[i]){
+                        for(int i = 0; i < numChars; i++){
+                            if(!inbounds(r+dr*i, c+dc*i, numRows, numCols) ||
+                                puzzle[r+dr*i][c+dc*i] != XMAS[i]){
                                     match = false;
-                                }
+                                    break;
                             }
                         }
                         if(match){
-                            std::cout << "found match, r: " << r << ", dr: " << dr 
-                                << ", c: " << c << ", dc: " << dc << "\n";
                             wordCount++;
                         }
                     }
@@ -119,8 +198,8 @@ int main(int argc, char* argv[])
     populate_puzzle(fileName, puzzle);
     
 
-    std::cout << "\nPuzzle Input\n";
-    print_puzzle(puzzle);
+    //std::cout << "\nPuzzle Input\n";
+    //print_puzzle(puzzle);
 
     int xmasCount1 = part01(puzzle);
 
