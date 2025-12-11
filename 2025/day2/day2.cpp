7caa033b8a8ed6a1fa929f52813870635a654511
willydlw <willydlw@comcast.net>
2025-12-11 07:10:59 -0700

    refactored part 1. Still needs testing.

--- 2025/day2/day2.cpp
+++ 2025/day2/day2.cpp
@@ -75,34 +75,21 @@ int getLesserExponent(int number)
     return static_cast<int>(std::floor(logVal));
 }
 
-/*
-    Assumptions:
-
-        The std::pair first value <= second value.
-
-    Problem Statement:
-        - None of the numbers have leading zeroes; 0101 isn't an ID at all.
-          Does not indicate if there may be an id 0.
-*/
-void part1(const std::vector<std::pair<int, int>> &idRanges)
+void setRangeParameters(int number, int& lowThreshold, int& startVal, int& increment)
 {
-    int invalidIDCount = 0;
-    for(const auto& r : idRanges){
-        std::cerr << "range first: " << r.first << ", second: " <<  r.second << "\n";
-
-        int lesserExponent = 0;
-        int greaterExponent = 0;
-        lesserExponent = getLesserExponent(r.first);
+        int lesserExponent = getLesserExponent(number);
 
         // Duplicate patterns only found in numbers with even number of digits 
         // lesserExponent must be odd. 
         //  Examples: 33 is duplicate, log10(33) is 1
         //            333 is not considered invalid, log10(333) is 2 
         //            6565 is invalid, log10(6565) is 3
-        if(lesserExponent & 0x1 == 0)
+        if( (lesserExponent & 0x1) == 0)
         {
             ++lesserExponent;
         }
+
+        lowThreshold = static_cast<int>(std::pow(10, lesserExponent));
 
         // Lesser exponent is an odd number
         // Odd number defined as 2n + 1
@@ -119,7 +106,7 @@ void part1(const std::vector<std::pair<int, int>> &idRanges)
         //   lesserExponent = 3 
         //   n = (3-1)/2 = 1 
         //   10^3 + 10^1 = 1000 + 10 = 1010
-        int startVal = static_cast<int>(std::pow(10, lesserExponent)) + static_cast<int>(std::pow(10, n));
+        startVal = static_cast<int>(std::pow(10, lesserExponent)) + static_cast<int>(std::pow(10, n));
 
         //  Increment for 2 digit numbers should be 11 
         //                4 digit numbers should be 101 
@@ -131,103 +118,63 @@ void part1(const std::vector<std::pair<int, int>> &idRanges)
         //  (1010, 1111, 1212, 1313, ..., 9898, 9999) are all invalid IDs 
 
         //  When lesser exponent is 5, we have 6 digit numbers starting at 100100 and incrmenting 10001
-        int increment = 
+        increment = static_cast<int>(std::pow(10, (lesserExponent+1)/2)) + 1;
 
-        std::cerr << "lesserExponent " << lesserExponent << ", startVal" << startVal<< "\n";
+        std::cerr << "lesserExponent " << lesserExponent << ", startVal :" << startVal 
+                << ", increment: " << increment << "\n";
 
+        std::cerr << "Press enter key to continue...";
+        std::cin.get();
+}
+
+/*
+    Assumptions:
+
+        The std::pair first value <= second value.
+
+    Problem Statement:
+        - None of the numbers have leading zeroes; 0101 isn't an ID at all.
+          Does not indicate if there may be an id 0.
+*/
+void part1(const std::vector<std::pair<int, int>> &idRanges)
+{
+    int invalidIDCount = 0;
+    
+
+    for(const auto& r : idRanges){
+
+        int lowThreshold, highThreshold;      
+        int increment;
+        int startVal;            
+
+        std::cerr << "range first: " << r.first << ", second: " <<  r.second << "\n";
+        setRangeParameters(r.first, lowThreshold, startVal, increment);
+        highThreshold = lowThreshold * 10;
+
+        int current = startVal;
+
+        while(current <= r.second){
+            if(current >= highThreshold){
+                setRangeParameters(current, lowThreshold, startVal, increment);
+                highThreshold = lowThreshold * 10;
+                current = startVal;
+                continue;
+            }
+
+            // TODO: This is not necessary if all startvalues and increments have been 
+            // correctly calculated. At this point we could just skip the check and increment
+            // the invalid id count if my other logic is correct. Will leave it in for now 
+            // to verify the patterns are repeated.
+            if(checkTwiceRepeated(current)){
+                invalidIDCount++;
+            }
+
+            current += increment;
+        }
     }
 
     std::cerr << "Invalid ID Count: " << invalidIDCount << "\n";
 }
-
-#if 0
-void part1(const std::vector<std::pair<int,int>> &ranges)
-{
-    int invalidCount = 0;
-    for( const auto& r : ranges){
-        std::cerr << "low: " << r.first << ", high: " << r.second << "\n";
-        int lowThreshold = 10;
-        int highThreshold = 100;
-        int thresholdIncrement = 1;
-        int increment = 1;
-
-        // TODO: increments 11, 101, 1001, 10001, ...
-
-        for(int val = r.first; val <= r.second;){
-
-            while(val > lowThreshold){
-                std::cerr << "\ndetected val > lowThreshold, val: " << val << ", lowThreshold: " << lowThreshold << "\n";
-                lowThreshold *= 10;
-                highThreshold *= 10;
-                std::cerr << "updated lowThreshold: " << lowThreshold << ", highThreshold: " << highThreshold << "\n";
-                std::cerr << "Press enter key to continue...";
-                std::cin.get();
-            }
-
-            if(val == lowThreshold){
-                std::cerr << "\ndetected val == lowThreshold, val: " << val << ", lowThreshold: " << lowThreshold << "\n";
-                
-                thresholdIncrement = lowThreshold / 100;
-                increment = lowThreshold + 1;
-                val = lowThreshold + thresholdIncrement;
-                
-                std::cerr << "updated val: " << val << ", thresholdIncrment: " 
-                    << thresholdIncrement << ", increment: " << increment << "\n";
-                std::cerr << "Press enter to continue...";
-                std::cin.get();
-                continue;
-            }
-
-            else if(val < lowThreshold){
-                std::cerr << "\ndetected val < lowThreshold\n";
-                std::cerr << "current val: " << val << ", low threshold: " << lowThreshold 
-                    << ", thresholdIncrement: " << thresholdIncrement 
-                    << ", increment: " << increment << "\n";
-                
-                val = lowThreshold + thresholdIncrement;
-                increment = lowThreshold + 1;
-                
-                
-                std::cerr << "updated val: " << val << ", low threshold: " << lowThreshold 
-                    << ", thresholdIncrement: " << thresholdIncrement 
-                    << ", increment: " << increment << "\n";
-                std::cerr << "Press enter key to continue...";
-                std::cin.get();
-                continue;
-            }
-            else if(val >= highThreshold){
-                std::cerr << "\ndetected val >= highThreshold\n";
-                std::cerr << "current val: " << val << ", low threshold: " << lowThreshold 
-                    << ", highThreshold: " << highThreshold 
-                    << ", thresholdIncrement: " << thresholdIncrement 
-                    << ", increment: " << increment << "\n";
-                lowThreshold *= 100;
-                highThreshold *= 100;
-                thresholdIncrement = lowThreshold / 100;
-                
-
-                std::cerr << "detected val >= highThreshold\n";
-                std::cerr << "updated  low threshold: " << lowThreshold 
-                    << ", highThreshold: " << highThreshold << "\n";
-                std::cerr << "Press enter key to continue...";
-                std::cin.get();
-                continue;
-            }
-
-            if(checkTwiceRepeated(val)){
-                //std::cerr << "repeating pattern detected: " << val << "\n";
-                invalidCount++;
-            }
-
-            val += increment;
-
-        }
-    }
-
-    std::cerr << "Part 1 answer, invalid ID count: " << invalidCount << "\n";
-}
-#endif
-
 
 int main(int argc, char* argv[])
 {
